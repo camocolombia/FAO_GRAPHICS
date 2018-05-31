bca6c336e89d5abe301d581735d974225349c16e
Chrystian Camilo Sosa Arango <chrystiam.sossa@gmail.com>
2018-05-31 16:34:16 +0100

    Add files via upload

--- FAO_GRAPHICS.R
+++ FAO_GRAPHICS.R
@@ -219,3 +219,189 @@ p3<-ggplot(FAO_MERGE, aes(x=Year, y=Em_Head, colour=Region, group = Region)) +
 ggsave(paste0("E:/Dropbox/Dropbox/Paper_PhD/FAO","/","LINE_EM_HEAD_SHEEP",".pdf"),p3,dpi=600,width =90,height=60,units = "cm",scale=1.2,limitsize = FALSE)
   #ggsave(paste0("E:/Dropbox/Dropbox/Paper_PhD/FAO","/","LINE_PLOT",".png"),p,dpi=600,width =90,height=60,units = "cm",scale=1.2,limitsize = FALSE)
 
+
+#########################################
+#VALUE $$$ IMPORT
+#########################################
+FAO3 <- read.csv("E:/Dropbox/Dropbox/Paper_PhD/FAO/FAOSTAT_data_5-31-2018_TRADE.csv",header = T,sep=",")
+FAO3 <- FAO3[which(FAO3$Item=="Sheep"),]
+FAO3 <- FAO3[which(FAO3$Element=="Import Value"),]
+
+FAO_data_TA <- as.data.frame(cbind(as.character(FAO3$Country),
+                                   as.numeric(as.character(FAO3$Year)),
+                                   as.numeric(as.character(FAO3$Value))
+))
+colnames(FAO_data_TA) <- c("Region","Year","Value")
+
+FAO_data_TA$Year <- as.character(FAO3$Year)
+FAO_data_TA$Year <- as.numeric(as.character(FAO_data_TA$Year))
+FAO_data_TA$Value <- as.numeric(as.character(FAO_data_TA$Value))
+
+
+
+
+#FAO_data_TA$Year <- year(as.Date(FAO_data_TA$Year,"%Y")) #it will gives only years Hope it helps to you! :)
+
+
+
+
+#class(FAO_data_TA$Region)
+#FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region=="World"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Eastern Africa"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Middle Africa"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Northern Africa"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Western Africa"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Southern Africa"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Northern America"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Central America"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Caribbean"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "South America"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Central Asia"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Eastern Asia"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Southern Asia"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "South-Eastern Asia"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Western Asia"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Eastern Europe"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Northern Europe"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Southern Europe"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Western Europe"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Australia & New Zealand"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Melanesia"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Polynesia"),]
+FAO_data_TA <- FAO_data_TA[which(FAO_data_TA$Region != "Ireland"),]
+
+
+FAO_data_TA$Value <- FAO_data_TA$Value*1000; FAO_data_TA$Value <- FAO_data_TA$Value/1000000
+
+
+p4<-ggplot(FAO_data_TA, aes(x=Year, y=Value,colour=Region, group = Region)) +
+  # geom_path( position = "identity") +
+  geom_line(size=4)+
+  #  geom_point()+
+  xlab("Year")+
+  #  ylab("Gigagrams of CH4") +
+  
+  
+  ylab("Million of USD (Imports)") +
+  
+  
+  #scale_colour_brewer(type = "qual", palette = 8) +
+  scale_colour_brewer(type = "qual", palette = "Set1")+
+  scale_x_continuous(breaks=seq(1961,2016,1),limits = c(1961,2016))+
+  #scale_y_continuous(breaks=seq(0,1300,100),limits = c(0,1300))+
+  theme(panel.background = element_rect(fill = "gray95"),
+        text=element_text(size=40),
+        axis.text.x  = element_text(size=40,colour="black",angle = 90, hjust = 1),
+        axis.text.y  = element_text(size=40,colour="black"),
+        legend.title=element_blank(),
+        legend.box = "horizontal",
+        # legend.text=element_text(size=rel(0.8)),
+        legend.title = element_text(size=60), 
+        legend.text = element_text(size=60),
+        legend.key.height = unit(5, "cm"),
+        legend.key.width = unit(5, "cm")
+        
+  )
+
+
+#+
+#geom_point(aes(color=Region))
+#p
+
+
+ggsave(paste0("E:/Dropbox/Dropbox/Paper_PhD/FAO","/","LINE_TA_I",".pdf"),p4,dpi=600,width =90,height=60,units = "cm",scale=1.2,limitsize = FALSE)
+#ggsave(paste0("E:/Dropbox/Dropbox/Paper_PhD/FAO","/","LINE_PLOT",".png"),p,dpi=600,width =90,height=60,units = "cm",scale=1.2,limitsize = FALSE)
+
+
+#########################################
+#VALUE $$$ EXPORT
+#########################################
+FAO4 <- read.csv("E:/Dropbox/Dropbox/Paper_PhD/FAO/FAOSTAT_data_5-31-2018_TRADE.csv",header = T,sep=",")
+FAO4 <- FAO4[which(FAO4$Item=="Sheep"),]
+FAO4 <- FAO4[which(FAO4$Element=="Export Value"),]
+
+FAO_data_TA_E <- as.data.frame(cbind(as.character(FAO4$Country),
+                                     as.numeric(as.character(FAO4$Year)),
+                                     as.numeric(as.character(FAO4$Value))
+))
+colnames(FAO_data_TA_E) <- c("Region","Year","Value")
+
+FAO_data_TA_E$Year <- as.character(FAO4$Year)
+FAO_data_TA_E$Year <- as.numeric(as.character(FAO_data_TA_E$Year))
+FAO_data_TA_E$Value <- as.numeric(as.character(FAO_data_TA_E$Value))
+
+
+
+
+#FAO_data_TA_E$Year <- year(as.Date(FAO_data_TA_E$Year,"%Y")) #it will gives only years Hope it helps to you! :)
+
+
+
+
+#class(FAO_data_TA_E$Region)
+#FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region=="World"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Eastern Africa"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Middle Africa"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Northern Africa"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Western Africa"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Southern Africa"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Northern America"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Central America"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Caribbean"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "South America"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Central Asia"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Eastern Asia"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Southern Asia"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "South-Eastern Asia"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Western Asia"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Eastern Europe"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Northern Europe"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Southern Europe"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Western Europe"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Australia & New Zealand"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Melanesia"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Polynesia"),]
+FAO_data_TA_E <- FAO_data_TA_E[which(FAO_data_TA_E$Region != "Ireland"),]
+
+
+FAO_data_TA_E$Value <- FAO_data_TA_E$Value*1000; FAO_data_TA_E$Value <- FAO_data_TA_E$Value/1000000
+
+
+p5<-ggplot(FAO_data_TA_E, aes(x=Year, y=Value,colour=Region, group = Region)) +
+  # geom_path( position = "identity") +
+  geom_line(size=4)+
+  #  geom_point()+
+  xlab("Year")+
+  #  ylab("Gigagrams of CH4") +
+  
+  
+  ylab("Million of USD (Exports)") +
+  
+  
+  #scale_colour_brewer(type = "qual", palette = 8) +
+  scale_colour_brewer(type = "qual", palette = "Set1")+
+  scale_x_continuous(breaks=seq(1961,2016,1),limits = c(1961,2016))+
+  #scale_y_continuous(breaks=seq(0,1300,100),limits = c(0,1300))+
+  theme(panel.background = element_rect(fill = "gray95"),
+        text=element_text(size=40),
+        axis.text.x  = element_text(size=40,colour="black",angle = 90, hjust = 1),
+        axis.text.y  = element_text(size=40,colour="black"),
+        legend.title=element_blank(),
+        legend.box = "horizontal",
+        # legend.text=element_text(size=rel(0.8)),
+        legend.title = element_text(size=60), 
+        legend.text = element_text(size=60),
+        legend.key.height = unit(5, "cm"),
+        legend.key.width = unit(5, "cm")
+        
+  )
+
+
+#+
+#geom_point(aes(color=Region))
+#p
+
+
+ggsave(paste0("E:/Dropbox/Dropbox/Paper_PhD/FAO","/","LINE_TA_E",".pdf"),p5,dpi=600,width =90,height=60,units = "cm",scale=1.2,limitsize = FALSE)
+#ggsave(paste0("E:/Dropbox/Dropbox/Paper_PhD/FAO","/","LINE_PLOT",".png"),p,dpi=600,width =90,height=60,units = "cm",scale=1.2,limitsize = FALSE)
+
